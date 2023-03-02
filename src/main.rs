a4ad00cae249bcc1383e7174ac4ea8386eceaec8
Jonathan Johnson <jon@khonsulabs.com>
2023-03-02 10:39:57 -0800

    Adding worker thread configuration options

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,26 @@ mod schema;
 mod webserver;
 
 use std::convert::Infallible;
+use std::num::NonZeroUsize;
 
 use bonsaidb::{cli::CommandLine, core::async_trait::async_trait, server::Backend};
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    CliBackend.run().await
+fn main() -> anyhow::Result<()> {
+    let worker_threads = std::env::var("WORKERS")
+        .ok()
+        .and_then(|workers| workers.parse::<usize>().ok())
+        .or_else(|| {
+            std::thread::available_parallelism()
+                .ok()
+                .map(NonZeroUsize::get)
+        })
+        .unwrap_or(16);
+    println!("Starting with {worker_threads} worker threads");
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .worker_threads(worker_threads)
+        .build()?
+        .block_on(CliBackend.run())
 }
 
 #[derive(Debug, Default)]
